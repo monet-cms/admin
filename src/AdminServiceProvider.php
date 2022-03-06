156fe3b829330453efb1dbd15bafb214743ec4bc
Kieron Sutton <sutton.kieron99@gmail.com>
2022-03-06 05:50:32 +0000

    Initial commit

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Monet\Framework\Admin;
+
+use Monet\Framework\Support\Services\Package;
+use Monet\Framework\Support\Services\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package->name('monet.admin');
+    }
+}
