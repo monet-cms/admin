8121671cfafedea782818db6192b0c853c29441e
Kieron Sutton <sutton.kieron99@gmail.com>
2022-03-06 07:28:31 +0000

    Initial commit

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Monet\Framework\Admin;
 
+use Livewire\Livewire;
+use Monet\Framework\Admin\Http\Livewire\Tiles\DummyTile;
 use Monet\Framework\Support\Services\Package;
 use Monet\Framework\Support\Services\ServiceProvider;
 
@@ -14,5 +16,15 @@ class AdminServiceProvider extends ServiceProvider
             ->hasRoute('web')
             ->hasViews()
             ->hasAssets();
+    }
+
+    protected function packageBooted(): void
+    {
+        $this->registerLivewireComponents();
+    }
+
+    protected function registerLivewireComponents(): void
+    {
+        Livewire::component('monet.admin::dummy-tile', DummyTile::class);
     }
 }
