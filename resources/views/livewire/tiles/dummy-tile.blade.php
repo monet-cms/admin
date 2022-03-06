7233a7a8fb60ea0b5424c9ea05cff8ec3e43ec5c
Kieron Sutton <sutton.kieron99@gmail.com>
2022-03-06 07:46:13 +0000

    Initial commit

--- resources/views/livewire/tiles/dummy-tile.blade.php
+++ resources/views/livewire/tiles/dummy-tile.blade.php
@@ -1,3 +1,3 @@
-<x-monet.dashboard::dashboard-tile :position="$position">
+<x-monet.dashboard::dashboard-tile-component :position="$position">
     <h1>Hello world</h1>
-</x-monet.dashboard::dashboard-tile>
+</x-monet.dashboard::dashboard-tile-component>
