7926183d53b53e06cb38bd6e1dd8d3bc06b1b04b
Kieron Sutton <sutton.kieron99@gmail.com>
2022-03-08 04:03:09 +0000

    Fixed errors

--- resources/views/components/side-nav.blade.php
+++ resources/views/components/side-nav.blade.php
@@ -1,13 +1,5 @@
 <div class="w-52 px-4">
     <div class="space-y-6">
-        <ul class="space-y-3">
-            <li class="uppercase font-semibold text-gray-400 select-none tracking-light">
-                Dashboard
-            </li>
-
-            <li>
-                <a href="#" class></a>
-            </li>
-        </ul>
+        {!! app('admin-menu')->render() !!}
     </div>
 </div>
--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -7,6 +7,7 @@ use Monet\Framework\Admin\Http\Livewire\Tiles\DummyTile;
 use Monet\Framework\Support\Services\Package;
 use Monet\Framework\Support\Services\ServiceProvider;
 use Spatie\Menu\Laravel\Facades\Menu;
+use Spatie\Menu\Laravel\Link;
 
 class AdminServiceProvider extends ServiceProvider
 {
@@ -21,11 +22,18 @@ class AdminServiceProvider extends ServiceProvider
 
     protected function packageBooted(): void
     {
-        $this->app->singleton('admin-menu', function() {
-            return Menu::new();
+        $this->app->singleton('admin-menu', function () {
+            return $this->createMenu();
         });
 
         $this->registerLivewireComponents();
+    }
+
+    protected function createMenu()
+    {
+        return Menu::new()
+            ->addClass('space-y-3')
+            ->add(Link::toRoute('admin.dashboard', 'Dashboard'));
     }
 
     protected function registerLivewireComponents(): void
